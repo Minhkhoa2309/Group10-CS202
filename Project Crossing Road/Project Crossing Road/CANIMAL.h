2c89c544df77bda4a728516881504d223117b5f2
kaga5477 <hieu.nguyenhoang.5477@gmail.com>
2020-11-16 21:55:56 +0700

    .h

--- Project Crossing Road/Project Crossing Road/CANIMAL.h
+++ Project Crossing Road/Project Crossing Road/CANIMAL.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef _canimal_h_
+#define _canimal_h_
+
+#include <iostream>
+
+using namespace std;
+
+class CANIMAL {
+	int mX, mY;
+public:
+	virtual void Move(int, int);
+	virtual void Tell();
+};
+
+class CDINAUSOR : public CANIMAL {
+public:
+	//...
+};
+
+class CBIRD : public CANIMAL {
+public:
+	//...
+};
+
+#endif
--- Project Crossing Road/Project Crossing Road/CVEHICLE.h
+++ Project Crossing Road/Project Crossing Road/CVEHICLE.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef _cvehicle_h_
+#define _cvehicle_h_
+
+#include <iostream>
+
+using namespace std;
+
+class CVEHICLE {
+	int mX, mY;
+public:
+	virtual void Move(int, int);
+	//...
+};
+
+class CTRUCK : public CVEHICLE {
+public:
+	//...
+};
+
+class CCAR : public CVEHICLE {
+public:
+	//...
+};
+
+#endif
