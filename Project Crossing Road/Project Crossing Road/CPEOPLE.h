09cc72b777b1ee5c11a76cacbabf5b733bf51734
kaga5477 <hieu.nguyenhoang.5477@gmail.com>
2020-11-16 21:01:13 +0700

    CPEOPLE.h
    copy cái .h vô thui

--- Project Crossing Road/Project Crossing Road/CPEOPLE.h
+++ Project Crossing Road/Project Crossing Road/CPEOPLE.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef _cpeople_h_
+#define _cpeople_h_
+
+#include <iostream>
+
+using namespace std;
+
+class CPEOPLE {
+private:
+	int mX, mY;
+	bool mState;
+public:
+	CPEOPLE();
+	void Up(int);
+	void Left(int);
+	void Right(int);
+	void Down(int);
+	bool isImpact(const CVEHICLE*&);
+	bool isImpact(const CANIMAL*&);
+	bool isFinish();
+	bool isDead();
+};
+
+#endif
