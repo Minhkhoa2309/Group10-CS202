94bc95c417ca2454fafbbfd70d9979d4272379bb
longpham1607 <63382848+longpham1607@users.noreply.github.com>
2020-11-18 07:15:29 +0700

    Mấy hàm chỉnh kích thước console

--- Project Crossing Road/Project Crossing Road/ConsoleWindow.h
+++ Project Crossing Road/Project Crossing Road/ConsoleWindow.h
@@ -1,1 +1,9 @@
 #pragma once
+#include<Windows.h>
+
+void resizeConsole(int width, int height);  // Long 
+void FixConsoleWindow(); // Long
+void SetColor(int backgound_color, int text_color); // Long
+void SetScreenBufferSize(SHORT width, SHORT height); // Long
+void ShowScrollbar(BOOL Show); // Long
+void DisableCtrButton(bool Close, bool Min, bool Max); // Long
